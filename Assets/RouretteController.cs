6e51f942f5e9e5260000296efd20d943293d1d37
tajima <programming.tajima@gmail.com>
2025-08-01 14:35:52 +0900

    work250801

--- Assets/RouretteController.cs
+++ Assets/RouretteController.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class RouretteController : MonoBehaviour
 {
     float rotSpeed = 0;  // 回転速度
-    bool is_rotating = false;
-    bool is_secondClick = false;
+    bool is_rotating = false; // 回転中か
+    bool is_secondClick = false; // 2回目のクリックか
 
     // 最初に一回だけ動く
     void Start()
@@ -20,16 +20,17 @@ public class RouretteController : MonoBehaviour
     void Update()
     {
         // Debug.Log("Update!");
-        // マウスが押されたら回転速度を設定する
+        // マウスが押されたら回転開始
+        // フラグを変更
         if (Input.GetMouseButtonDown(0))
         {
             if (is_rotating == false)
             {
-                this.rotSpeed = 10;
+                this.rotSpeed = 20;
                 is_rotating = true;
                 Debug.Log("is_rotating:" + is_rotating);
             }
-            // 回転中なら、2クリックに変更
+            // 回転中なら、フラグ2クリックに変更
             else
             {
                 is_secondClick = true;
@@ -39,10 +40,19 @@ public class RouretteController : MonoBehaviour
         // 2クリックなら、減速する
         if (is_secondClick == true)
         {
-            this.rotSpeed *= 0.96f;
+            this.rotSpeed *= 0.98f;
         }
 
         transform.Rotate(0, 0, this.rotSpeed);
+
+        // 回転が遅くなってきたら、止める
+        // フラグを初期値へ戻す
+        if (this.rotSpeed < 0.2f)
+        {
+            this.rotSpeed = 0;
+            is_rotating = false;
+            is_secondClick = false;
+        }
 
         // transform.Translate(0.01f, 0, 0, Space.World);
     }
